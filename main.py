f6f50a0774f8071b7f8b09754d034f03afa4dd7c
Hiago Rios <hiagoriosc@gmail.com>
2019-08-21 20:55:22 -0300

    Oscilador harmônico funcionando

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 '''
-Problema de valor inicial do pendulo:
-{ (d²Θ)/dt² = (-g/L)*senΘ
-{ Θ(t0) = Θ0
+t -> tempo
+Θ -> angulo entre a "corda" do pendulo e a posicao de repouso
+Θ0 -> angulo inicial e também máximo que o pendulo atinge
+
+(d²Θ)/dt² = (-g/L)*senΘ -> equacao do oscilador harmonico
+Θ(t) = Θ0*cos(raiz(g/l)*t)
+
+x(teta) = l*sen(teta)
+y(teta) = l*cos(teta)
 '''
 
-from sympy import lambdify, sin
-from sympy.abc import x
-from rungekutta import rungeKutta
+import numpy as np
+import matplotlib.pyplot as plt
 
-def show ():
-    print('Not implemented yet')
+# Inicializacao das variaveis #
+g = 9.8
+L = 2
+n = 1000
+t = 100
+teta = {}
+w = {}
 
-g = 9.6
-L = float (input('Digite L: '))
-t0 = float (input('Digite o Teta inicial: '))
-n = int(input("Digite o número de subdivisões: "))
+teta[0] = np.pi/6
+w[0] = 0
+x = np.linspace(0, n-1, n)
 
-f = lambdify(x, (-g/L)*sin(x))
+################################
 
-for i in range (0, 100):
-    print(rungeKutta(i, 20, n, t0, f))
+def rungeKutta(n, teta, w, h):
+	k1, k2, k3, k4 = 0, 0, 0, 0
 
-print(f(t0))
+	i = 0
+	while i < n - 1:
+		k1 = h*w[i]
+		k2 = h*(w[i] + k1/2)
+		k3 = h*(w[i] + k2/2)
+		k4 = h*(w[i] + k3)
+
+		teta[i + 1] = teta[i] + (k1 + 2*k2 + 2*k3 + k4)*(h/6)
+
+		k1 = (-g/L)*np.sin(teta[i])*h
+		k2 = (-g/L)*np.sin(teta[i] + k1/2)*h
+		k3 = (-g/L)*np.sin(teta[i] + k2/2)*h
+		k4 = (-g/L)*np.sin(teta[i] + k3)*h
+
+		w[i + 1] = w[i] + (k1 + 2*k2 + 2*k3 + k4)*(h/6)
+
+		i = i + 1
+######################################################
+
+rungeKutta(n, teta, w, t/n)
+teta = np.array([v for v in teta.values()])
+w = np.array([v for v in w.values()])
+plt.plot(x, teta, 'r--')
+plt.plot(x, w, 'b--')
+plt.show()

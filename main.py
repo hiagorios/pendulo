c9f6d8d350adfb6f9b4a59e809815b620b85810f
Hiago Rios <hiagoriosc@gmail.com>
2019-08-22 21:41:12 -0300

    Melhoramento do código e incrementos no relatório

--- main.py
+++ main.py
@@ -23,9 +23,8 @@ w = {}
 
 teta[0] = np.pi/6
 w[0] = 0
-x = np.linspace(0, n-1, n)
 
-################################
+######################################################
 
 def rungeKutta(n, teta, w, h):
 	k1, k2, k3, k4 = 0, 0, 0, 0
@@ -49,7 +48,21 @@ def rungeKutta(n, teta, w, h):
 		i = i + 1
 ######################################################
 
-def pendulo(x, y):
+def oscilacao():
+	x = np.linspace(0, n-1, n)
+	plt.plot(x, teta, 'r--')
+	plt.plot(x, w, 'b--')
+	plt.legend(['Teta', 'Omega'])
+	plt.show()
+
+######################################################
+
+def pendulo():
+	x = np.zeros(n, dtype=float)
+	y = np.zeros(n, dtype=float)
+	for i in range (n):
+		x[i] = L*np.sin(teta[i])
+		y[i] = L*np.cos(teta[i])
 	graph = plt.subplot()
 	i = 0
 	plt.grid()
@@ -57,6 +70,7 @@ def pendulo(x, y):
 	while i < n:
 		graph.scatter(x[i], -y[i], color='black', s=200)
 		plt.pause(0.0001)
+		# nao consegui usar as funcoes de update do grafico
 		graph.scatter(x[i], -y[i], color='white', s = 300)
 		i = i + 10
 
@@ -66,13 +80,5 @@ rungeKutta(n, teta, w, t/n)
 teta = np.array([v for v in teta.values()])
 w = np.array([v for v in w.values()])
 
-x = np.zeros(n, dtype=float)
-y = np.zeros(n, dtype=float)
-for i in range (n):
-	x[i] = L*np.sin(teta[i])
-	y[i] = L*np.cos(teta[i])
-
-pendulo(x, y)
-#plt.plot(x, teta, 'r--')
-#plt.plot(x, w, 'b--')
-#plt.show()
+oscilacao()
+pendulo()
